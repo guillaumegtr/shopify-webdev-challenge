21282967303203ed33c499ce8d3f67a1163b5901
guillaumegtr <36165858+guillaumegtr@users.noreply.github.com>
2021-05-06 11:56:20 -0400

    [Fix] Nomination UI 🚧 (#9)
    - fix layout when full screen
    - adjust styling

--- src/components/pretty-share.tsx
+++ src/components/pretty-share.tsx
@@ -7,16 +7,19 @@ interface PrettyShareProps {
 
 const PrettyShare = (props: PrettyShareProps) => {
   const { sharedMovies } = props;
-  console.log(sharedMovies);
 
   return (
     <>
-      <div className="flex-row">
+      <div className="flex-row mt-1">
         <h1>Here is your friend's Nomination List 🍿</h1>
       </div>
-      <div className="flex-row mt-1">
+      <div className="flex-row my-1">
         {sharedMovies.length > 0 ? (
-          sharedMovies.map((m, i) => <MovieCard key={i} movie={m} />)
+          <div className="cards-container flex-row">
+            {sharedMovies.map((m, i) => (
+              <MovieCard key={i} movie={m} />
+            ))}
+          </div>
         ) : (
           <p>Shared movies not found ... Try again please 🤓</p>
         )}
